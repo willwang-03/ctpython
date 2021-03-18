99e1cdf4cdb323c7608243284e1b38b59be887d0
willwang-03 <80619676+willwang-03@users.noreply.github.com>
2021-03-18 16:56:45 +0800

    Add files via upload

---
+++ Hw1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 16:37:42 2021
+
+@author: user
+"""
+
+for a in range (5,1,-1):
+    for b in range(0,a):
+        print("*",end='')
+    print()
+
+for c in range (1,6):
+    for d in range(0,c):
+        print("*",end='')            
+    print()
---
+++ Hw2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 17:24:54 2021
+
+@author: user
+"""
+
+start = int(input('初始值:'))
+end = int(input('終止值:'))
+
+print('4的倍數有:',end=" ")
+for i in range(start,end+1):
+    if i % 4 == 0:
+        print(i,sep=',',end=' ')
+        
+print()        
+print('質數有:',end=" ")
+
+for j in range(1,end+1):
+    count = 0
+    for k in range(2,j):
+        if j % k == 0:
+            count += 1
+    if count == 0 and j != 1:
+        print(j,end=' ')
+
+    
+print()
+print('程式執行完畢')    
