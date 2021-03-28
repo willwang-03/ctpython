1754385f4daded20860e73c6c38a61e2363478b7
willwang-03 <80619676+willwang-03@users.noreply.github.com>
2021-03-28 10:53:34 +0800

    Add files via upload

---
+++ Hw1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 16:37:42 2021
+
+20210314 Hw1
+*****
+****
+***
+**
+*
+**
+***
+****
+*****
+
+@author: user
+"""
+
+for a in range (5,1,-1):
+    for b in range(0,a):
+        print("*",end='')
+    print()
+
+for c in range (1,6):
+    for d in range(0,c):
+        print("*",end='')            
+    print()
